04788a99795ac6bba6f95e88ca60ff52c60e7e06
Rifandye <rilfransyah@gmail.com>
2024-03-17 02:25:25 +0700

    style: styling for navigation interface

--- followpoint-app/src/app/page.jsx
+++ followpoint-app/src/app/page.jsx
@@ -1,16 +1,17 @@
 import EventCard from "@/components/eventCard";
-import Image from "next/image";
+import DropdownSidebar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="">
+      <DropdownSidebar />
       <div>Hello World</div>
       <div>
-        < EventCard />
-        < EventCard />
-        < EventCard />
-        < EventCard />
-        < EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
       </div>
     </main>
   );
