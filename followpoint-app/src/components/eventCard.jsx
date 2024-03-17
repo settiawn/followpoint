8376f57d28f488211cfb5eec76d03980986fce0e
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2024-03-18 03:01:14 +0700

    chore: fetch data to frontend & add assets

--- followpoint-app/src/components/eventCard.jsx
+++ followpoint-app/src/components/eventCard.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
-export default function EventCard() {
+export default function EventCard({data}) {
   return (
     <div className="m-1 border-sky-100 border">
-      <div>Event title</div>
-      <div>event excerpt</div>
-      <div>event tags: music, food, etc</div>
-      <div>event date</div>
-      <Link href={"event/nama-event"}>
+      <div>{data.title}</div>
+      <div>{data.description}</div>
+      <div>Date : {data.date}</div>
+      <div>Organizer : {data.organizer}</div>
+      <div><img src={data.thumbnail} alt="" className="w-96 h-96" /></div>
+      <Link href={`/event/${data.slug}`}>
         <div className="hover:cursor-pointer hover:bg-slate-500">go to event details</div>
       </Link>
     </div>
