3f5a84f82f77b6475096792cb1d46d7abe7379dd
balanced-B <ariobimo.mp@gmail.com>
2024-03-19 04:16:36 +0700

    template checkout 1

--- followpoint-app/src/app/event/[slug]/buy/page.jsx
+++ followpoint-app/src/app/event/[slug]/buy/page.jsx
@@ -5,136 +5,141 @@ import { useEffect, useState } from "react";
 import Script from "next/script";
 
 export default function EventBuyTicketPage({ params }) {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [input, setInput] = useState({});
-  const [totalPrice, setTotalPrice] = useState(0);
+	const [data, setData] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [input, setInput] = useState({});
+	const [totalPrice, setTotalPrice] = useState(0);
 
-  function inputHandler(event) {
-    const { name, value } = event.target;
-    setInput((prevInput) => ({
-      ...prevInput,
-      [name]: parseInt(value, 10),
-    }));
-  }
+	function inputHandler(event) {
+		const { name, value } = event.target;
+		setInput((prevInput) => ({
+			...prevInput,
+			[name]: parseInt(value, 10),
+		}));
+	}
 
-  const handleIncrement = (name) => {
-    setInput((prevInput) => ({
-      ...prevInput,
-      [name]: (parseInt(prevInput[name]) || 0) + 1,
-    }));
-  };
+	const handleIncrement = (name) => {
+		setInput((prevInput) => ({
+			...prevInput,
+			[name]: (parseInt(prevInput[name]) || 0) + 1,
+		}));
+	};
 
-  const handleDecrement = (name) => {
-    setInput((prevInput) => ({
-      ...prevInput,
-      [name]: Math.max((parseInt(prevInput[name]) || 0) - 1, 0),
-    }));
-  };
+	const handleDecrement = (name) => {
+		setInput((prevInput) => ({
+			...prevInput,
+			[name]: Math.max((parseInt(prevInput[name]) || 0) - 1, 0),
+		}));
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	}, []);
 
-  useEffect(() => {
-    if (!loading) {
-      let totalPrice = 0;
-      data.tickets.forEach((ticket) => {
-        totalPrice += (input[ticket.ticketId] || 0) * ticket.price;
-      });
-      setTotalPrice(totalPrice);
-    }
-  }, [input, data, loading]);
+	useEffect(() => {
+		if (!loading) {
+			let totalPrice = 0;
+			data.tickets.forEach((ticket) => {
+				totalPrice += (input[ticket.ticketId] || 0) * ticket.price;
+			});
+			setTotalPrice(totalPrice);
+		}
+	}, [input, data, loading]);
 
-  const fetchData = async () => {
-    const res = await getEventDetails(params.slug);
-    setData(res);
-    setLoading(false);
-  };
+	const fetchData = async () => {
+		const res = await getEventDetails(params.slug);
+		setData(res);
+	};
 
-  const handlePayButton = async (e) => {
-    e.preventDefault();
-    console.log("clicked", input, totalPrice);
-    const response = await createTransaction(input, totalPrice, data._id);
+	const handlePayButton = async (e) => {
+		e.preventDefault();
+		console.log("clicked", input, totalPrice);
+		const response = await createTransaction(input, totalPrice, data._id);
 
-    console.log(response.transactionToken, "<<< transaction token");
-    console.log("test");
+		console.log(response.transactionToken, "<<< transaction token");
+		console.log("test");
 
-    window.snap.pay(response.transactionToken, {
-      onSuccess: function (result) {
-        //fetch patch
-        payTransaction(response.orderId);
-      },
-    });
-  };
+		window.snap.pay(response.transactionToken, {
+			onSuccess: function (result) {
+				//fetch patch
+				payTransaction(response.orderId);
+			},
+		});
+	};
 
-  console.log(input);
+	console.log(input);
 
-  return (
-    <>
-      <Script
-        src="https://app.sandbox.midtrans.com/snap/snap.js"
-        data-client-key={process.env.MIDTRANS_SERVER_KEY}
-        onLoad={() => {
-          console.log("Script has loaded");
-        }}
-      />
+	return (
+		<>
+			<Script
+				src="https://app.sandbox.midtrans.com/snap/snap.js"
+				data-client-key={process.env.MIDTRANS_SERVER_KEY}
+			/>
 
-      <main>
-        <div>Ini event buy ticket page page</div>
-        <div>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <>
-              <form action="" className="flex flex-col">
-                {data.tickets.map((x, i) => {
-                  return (
-                    <>
-                      <input
-                        type="number"
-                        className="text-black boerder-lg"
-                        name={x.ticketId}
-                        onChange={inputHandler}
-                        value={input[x.ticketId] || 0}
-                      />
-                      <span>
-                        price: {x.price}, stock : {x.stock}
-                      </span>
-                      <div>
-                        <div
-                          onClick={() => handleDecrement(x.ticketId)}
-                          disabled={input[x.ticketId] <= 0}
-                          className="hover:bg-sky-500 hover:cursor-pointer"
-                        >
-                          decrement
-                        </div>
-                        <div
-                          onClick={() => {
-                            handleIncrement(x.ticketId);
-                          }}
-                          className="hover:bg-sky-500 hover:cursor-pointer"
-                        >
-                          increment
-                        </div>
-                      </div>
-                    </>
-                  );
-                })}
-                <div>total price : {totalPrice}</div>
-                <button
-                  className="bg-blue-200"
-                  type="submit"
-                  onClick={handlePayButton}
-                  disabled={totalPrice === 0}
-                >
-                  Pay
-                </button>
-              </form>
-            </>
-          )}
-        </div>
-      </main>
-    </>
-  );
+			<main className="container mx-auto p-4 bg-[rgba(27,29,34,1)]">
+				<h1 className="text-3xl font-bold mb-4 text-white">Checkout</h1>
+				{loading ? (
+					<div>Loading...</div>
+				) : (
+					<form className="grid gap-4">
+						{data.tickets.map((ticket) => (
+							<div key={ticket.ticketId} className="bg-black p-4 rounded-md">
+								<div className="text-white mb-2">
+									<span>{ticket.ticketId}</span>
+									<p>Price: {ticket.price}</p>
+									<p>Stock: {ticket.stock}</p>
+								</div>
+								<div className="flex items-center justify-between text-white">
+									<table>
+										<tbody>
+											<tr>
+												<td>
+													<button
+														type="button"
+														onClick={() => handleDecrement(ticket.ticketId)}
+														disabled={input[ticket.ticketId] <= 0}
+													>
+														-
+													</button>
+												</td>
+												<td>
+													<input
+														type="number"
+														name={ticket.ticketId}
+														value={input[ticket.ticketId] || 0}
+														onChange={inputHandler}
+														className="text-black w-6 text-center justify-center items-center"
+													/>
+												</td>
+												<td>
+													<button
+														type="button"
+														onClick={() => handleIncrement(ticket.ticketId)}
+													>
+														+
+													</button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						))}
+						<div className="text-white">Total Price: {totalPrice}</div>
+						<button
+							type="submit"
+							className="bg-yellow-500 text-black py-2 px-4 mt-4 rounded-md"
+							onClick={handlePayButton}
+							disabled={totalPrice === 0}
+						>
+							Pay
+						</button>
+					</form>
+				)}
+			</main>
+		</>
+	);
 }
