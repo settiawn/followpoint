f1fc24cc3d97f47e31890a68c3d3bae234408e07
Rifandye <rilfransyah@gmail.com>
2024-03-19 03:31:20 +0700

    fix: fixing styling footer

--- followpoint-app/src/components/footer.jsx
+++ followpoint-app/src/components/footer.jsx
@@ -1,75 +1,67 @@
 export default function Footer() {
-	return (
-		<footer className="footer bg-primary text-primary-content">
-			<div className="container mx-auto flex flex-col justify-center items-center p-10 h-full">
-				<div className="flex items-center mb-4">
-					<img
-						src="/FOOTER_1-removebg-preview.png"
-						alt="Logo"
-						className="w-16 h-auto"
-					/>
+  return (
+    <footer className="footer bg-primary text-primary-content">
+      <div className="container mx-auto flex flex-col justify-center items-center p-10 h-full">
+        <div className="flex items-center">
+          <img
+            src="/FOOTER_1-removebg-preview.png"
+            alt="Logo"
+            className="w-16 h-auto"
+          />
 
-					<img
-						src="/Logo_Concept_2.png"
-						alt="Logo"
-						className="w-20 h-auto mr-10 ml-10"
-					/>
+          <img //ini harus aga gede
+            src="/Banner-Render-Concept.png"
+            alt="Logo"
+            className="w-32 h-auto mr-10 ml-10"
+          />
 
-					<img
-						src="/Banner-Render-Concept-footer.png"
-						alt="Logo"
-						className="w-16 h-auto"
-					/>
-				</div>
-				<p className="font-light text-sm text-gray-500">
-					Follow Point Enternaiment, Ltd <br />
-					Leading the Industry since 1994
-				</p>
-				<p className="font-light text-sm text-gray-500">
-					Copyright © 2023 - All right reserved
-				</p>
-
-				<div className="grid grid-flow-col gap-4">
-					<a href="#!" role="button">
-						<span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#d7bf03]">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-								<path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-							</svg>
-						</span>
-					</a>
-					<a>
-						<span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#d7bf03]">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								className="fill-current"
-							>
-								<path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-							</svg>
-						</span>
-					</a>
-					<a href="#!" role="button">
-						<span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#d7bf03]">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-								<path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-							</svg>
-						</span>
-					</a>
-					<a href="#!" role="button">
-						<span class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-[#d7bf03]">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="currentColor"
-								viewBox="0 0 512 512"
-							>
-								<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
-							</svg>
-						</span>
-					</a>
-				</div>
-			</div>
-		</footer>
-	);
+          <img
+            src="/FOOTER_2-removebg-preview.png"
+            alt="Logo"
+            className="w-24 h-auto"
+          />
+        </div>
+        <div className="grid grid-flow-col gap-4">
+          <a role="button">
+            <span class="[&>svg]:h-4 [&>svg]:w-4 [&>svg]:fill-[#d7bf03]">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+              </svg>
+            </span>
+          </a>
+          <a role="button">
+            <span class="[&>svg]:h-4 [&>svg]:w-4 [&>svg]:fill-[#d7bf03]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </span>
+          </a>
+          <a role="button">
+            <span class="[&>svg]:h-4 [&>svg]:w-4 [&>svg]:fill-[#d7bf03]">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+              </svg>
+            </span>
+          </a>
+          <a role="button">
+            <span class="[&>svg]:h-4 [&>svg]:w-4 [&>svg]:fill-[#d7bf03]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 512 512"
+              >
+                <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+              </svg>
+            </span>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
