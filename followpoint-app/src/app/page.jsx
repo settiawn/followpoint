0c1d6bd2f0accc0a31b8b2a8a5e2fc7244cddbe8
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2024-03-18 10:47:16 +0700

    fix: data fetching home, JSON data structure

--- followpoint-app/src/app/page.jsx
+++ followpoint-app/src/app/page.jsx
@@ -1,18 +1,27 @@
 import EventCard from "@/components/eventCard";
 import DropdownSidebar from "@/components/navbar";
 
-const getAllEventData = await fetch(
-  process.env.NEXT_PUBLIC_BASE_URL + "/api/events",
-  {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
+export const getAllEventData = async () => {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_BASE_URL + "/api/events",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Error!");
   }
-);
+
+  const { data } = await response.json();
+  return data;
+} 
 
 export default async function Home() {
-  const { data } = await getAllEventData.json();
+  const data = await getAllEventData();
 
   return (
     <main className="">
